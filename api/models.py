9ebca65615425f3edec429b1041e09447570a307
araviindm <araviindm@gmail.com>
2023-06-15 20:44:28 +0530

    Models, Default view and Admin configuration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Todo(models.Model):
+    time_stamp = models.DateTimeField(default=timezone.now, editable=False, blank=False, null=False)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.CharField(max_length=1000, blank=False, null=False)
+    due_date = models.DateTimeField(blank=True, null=False)
+
+    class Tag(models.Model):
+        name = models.CharField(max_length=255)
+
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    class StatusTypes(models.TextChoices):
+        OPEN = "OPEN"
+        WORKING = "WORKING"
+        DONE = "DONE"
+        OVERDUE = "OVERDUE"
+
+    status = models.CharField(max_length=7,
+                              choices=StatusTypes.choices,
+                              default=StatusTypes.OPEN,
+                              blank=False,
+                              null=False
+                              )
--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        {
+            'Endpoint': '/todo/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of todos'
+        },
+        {
+            'Endpoint': '/todo/id/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single todo object'
+        },
+        {
+            'Endpoint': '/todo/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new todo with data sent in post request'
+        },
+        {
+            'Endpoint': '/todo/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Updates an existing note with data sent in put request'
+        },
+        {
+            'Endpoint': '/todo/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an exiting todo'
+        },
+    ]
+    return Response(routes)
